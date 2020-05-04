f9354d6878a19cb9b9aa64a2a0784708d5c7e20e
kitpao <kitpao91@gmail.com>
2020-05-04 22:43:28 +0000

    Take out parameters from protect_from_forgery so it works in rails server and add config to preview

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "3000-aa1ef320-84d8-4f7e-b37b-f96291fefe47.ws-us02.gitpod.io"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
